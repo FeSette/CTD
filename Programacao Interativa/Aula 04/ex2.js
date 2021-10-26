64655e79041c790bf648644a42e9ee1ee40f09d0
Felipe Sette <felipe_sette@outlook.com>
2021-10-25 22:48:56 -0300

    Aula 04

---
+++ Programacao Interativa/Aula 04/ex2.js
@@ -0,0 +1,10 @@
+let nome = "Felipe";
+let idade = 25;
+let usuarioAtivo = true;
+
+console.log("Nome: ");
+console.log(nome);
+console.log("Idade: ");
+console.log(idade);
+console.log("O usuário está ativo? ");
+console.log(usuarioAtivo);
---
+++ Programacao Interativa/Aula 04/imc.js
@@ -0,0 +1,8 @@
+let nomeSobrenome = "José da Silva";
+let idade = 27;
+let peso = 83.5;
+let altura = 1.70;
+let plano = "Ouro";
+let IMC = peso/(altura*altura);
+
+console.log(nomeSobrenome + " tem " + idade + ' anos e seu índice de massa corporal é de ' + IMC.toFixed(0));
